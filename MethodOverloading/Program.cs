460828fcfb268e9ac796a1464ff449cae77ec7db
TaurusA <realtaurus97@gmail.com>
2024-01-28 16:10:29 -0600

    The method overload exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var sum = Add(3, 4);
+            Console.WriteLine(sum);
+
+            var doubleSum = Add(8.1, 5.9);
+            Console.WriteLine(doubleSum);
+
+            var decimalSum = Add(7.54m, 1.98m);
+            Console.WriteLine(decimalSum);
+
+
+
+            var dollarOne = Add(3, 2, true);
+            Console.WriteLine(dollarOne);
+
+            var dollarTwo = Add(0, 1, true);
+            Console.WriteLine(dollarTwo);
+
+            var dollarThree = Add(8, 8, true);
+            Console.WriteLine(dollarThree);
+
+            var dollarFour = Add(4, 4, false);
+            Console.WriteLine(dollarFour);
+
+
+        }
+
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+        public static double Add(double a, double b)
+        {
+            return (a + b);
+        }
+        public static decimal Add(decimal a, decimal b)
+        {
+            return (a + b);
+        }
+
+        public static string Add(int a, int b, bool yesDollar)
+        {
+            if (yesDollar && (a + b != 1))
+            {
+                return ($"{a + b}$");
+            }
+            else if (yesDollar && (a + b) == 1)
+            {
+                return ($"{a + b}$");
+            }
+            else
+            {
+                return ("Thats good money.");
+            }
         }
     }
 }
